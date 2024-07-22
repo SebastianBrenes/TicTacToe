1add63aacc94f22e9df0b6616c847629c631129e
Sebastian Rodriguez <sebastianrodrbrens@gmail.com>
2024-07-22 07:51:05 -0600

    Idea of game layout

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 const Gameboard = function () {
     this.board = 
-    ['', '', 'x',
-     '', '', 'x',
-     '', '', 'x'];
+    ['', '', '',
+     '', '', '',
+     '', '', ''];
 
     this.checkWin = function (char, name) {
         const winCombinations = [
@@ -45,13 +45,47 @@ function createPlayer (name, char) {
     return { playerName, char, getScore, addScore };
 }
 
-const player1 = createPlayer("Juancito", 'x');
-const player2 = createPlayer("Jacinto", 'O');
+// const player1 = createPlayer("Juancito", 'x');
+// const player2 = createPlayer("Jacinto", 'O');
 
-let tictactoe = new Gameboard();
+// let tictactoe = new Gameboard();
 
-tictactoe.checkWin(player1.char, player1.playerName)
-
-
+// tictactoe.checkWin(player1.char, player1.playerName)
 
 
+function promptPlayers() {
+    let main = document.querySelector('main');
+    main.innerHTML = '';
+    let contentPrompt = `   
+        <div id="custom-prompt">
+            <div class="prompt-content">
+                <label for="first-player">Enter name of Player 1:</label>
+                <input type="text" id="first-player" class="inputText">
+            </div>
+            <div class="prompt-content">
+                <label for="second-player">Enter name of Player 2:</label>
+                <input type="text" id="second-player" class="inputText">
+            </div>
+            <button id="submit-button">Submit</button>
+        </div>
+        `;
+
+    main.innerHTML = contentPrompt;
+
+    let buttonPlayerSelector = document.getElementById("submit-button");
+    buttonPlayerSelector.addEventListener('click', startGame);
+}
+
+function startGame() {
+    const inputPlayer1 = document.getElementById("first-player").value;
+    const inputPlayer2 = document.getElementById("second-player").value;
+    main.innerHTML = '';
+    console.log('Player 1:', inputPlayer1);
+    console.log('Player 2:', inputPlayer2);
+}
+
+let main = document.querySelector('main');
+
+let startButton = document.getElementById("start-button");
+
+startButton.addEventListener('click', promptPlayers);
