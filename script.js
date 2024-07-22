d78d8ce5c38b3008398898e6318418886ef5675d
Sebastian Rodriguez <sebastianrodrbrens@gmail.com>
2024-07-22 15:56:36 -0600

    Add game mechanics to the gameboard

--- script.js
+++ script.js
@@ -45,12 +45,7 @@ function createPlayer (name, char) {
     return { playerName, char, getScore, addScore };
 }
 
-// const player1 = createPlayer("Juancito", 'x');
-// const player2 = createPlayer("Jacinto", 'O');
 
-// let tictactoe = new Gameboard();
-
-// tictactoe.checkWin(player1.char, player1.playerName)
 
 
 function promptPlayers() {
@@ -60,13 +55,13 @@ function promptPlayers() {
         <div id="custom-prompt">
             <div class="prompt-content">
                 <label for="first-player">Enter name of Player 1:</label>
-                <input type="text" id="first-player" class="inputText">
+                <input type="text" id="first-player" class="inputText" required>
             </div>
             <div class="prompt-content">
                 <label for="second-player">Enter name of Player 2:</label>
-                <input type="text" id="second-player" class="inputText">
+                <input type="text" id="second-player" class="inputText" required>
             </div>
-            <button id="submit-button">Submit</button>
+            <button type="submit" id="submit-button">Submit</button>
         </div>
         `;
 
@@ -79,29 +74,59 @@ function promptPlayers() {
 function startGame() {
     const inputPlayer1 = document.getElementById("first-player").value;
     const inputPlayer2 = document.getElementById("second-player").value;
+    const player1 = createPlayer(inputPlayer1, 'X');
+    const player2 = createPlayer(inputPlayer2, 'O');
+    let turn = 0;
+
+    // let tictactoe = new Gameboard();
+
+    // tictactoe.checkWin(player1.char, player1.playerName)
+
     main.innerHTML = '';
-    // <p class="last-winner">Player... won</p>
     let contentPrompt = `   
             <h1 id="header-game">Tic Tac Toe!</h1>
             <div id="p-information">
                 <p class="score player1">${inputPlayer1} Score: 0pts</p>
                 <p class="score player2">${inputPlayer2} Score: 0pts</p>
+                <p class="last-winner">Player ${inputPlayer1} starts</p>
             </div>
             <section id="gameContainer">
-                <div class="container"></div>
-                <div class="container"></div>
-                <div class="container"></div>
-                <div class="container"></div>
-                <div class="container"></div>
-                <div class="container"></div>
-                <div class="container"></div>
-                <div class="container"></div>
-                <div class="container"></div>
+                <div data-index="0" class="container"></div>
+                <div data-index="1" class="container"></div>
+                <div data-index="2" class="container"></div>
+                <div data-index="3" class="container"></div>
+                <div data-index="4" class="container"></div>
+                <div data-index="5" class="container"></div>
+                <div data-index="6" class="container"></div>
+                <div data-index="7" class="container"></div>
+                <div data-index="8" class="container"></div>
             </section>
     `;
     main.innerHTML = contentPrompt;
-    
+    let gameContainer = document.getElementById("gameContainer");
+    gameContainer.addEventListener('click', (e) => {
+        if (turn === 0) {
+            if (e.target.className == "container") {
+                if (e.target.textContent === "") {
+                    e.target.textContent = player1.char;
+                    turn++;
+                }
+
+            }
+
+        } else {
+            if (e.target.className == "container") {
+                if (e.target.textContent === "") {
+                    e.target.textContent = player2.char;
+                    turn--;
+                }
+            }
+        }
+
+    }) 
 }
+
+
 
 let main = document.querySelector('main');
 
